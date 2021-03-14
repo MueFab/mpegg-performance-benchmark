feb21f3255048cc418028e64a5eeb98c82cf581d
Jan Voges <voges@tnt.uni-hannover.de>
2021-03-14 14:32:40 +0100

    Add set_mpegg_gidb_prefix.sh

---
+++ set_mpegg_gidb_prefix.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+readonly self="${0}"
+readonly self_name="${self##*/}"
+
+# Usage
+if [[ ${#} -ne 1 ]]; then echo "usage: ${0} <prefix>"; exit 1; fi
+readonly prefix="${1}"
+if [[ ${prefix} != */ ]]; then prefix="${prefix}/"; fi # append '/' if needed
+readonly git_root_dir="$(git rev-parse --show-toplevel)"
+
+# Prefix prepend
+safe_replacement=$(printf '%s\n' "${prefix}" | sed 's/[\&/]/\\&/g')
+sed --in-place "s/^/${safe_replacement}/g" "${git_root_dir}/bam_files.txt"
+echo "[${self_name}] updated paths: ${git_root_dir}/bam_files.txt"
+sed --in-place "s/^/${safe_replacement}/g" "${git_root_dir}/fastq_gz_files.txt"
+echo "[${self_name}] updated paths: ${git_root_dir}/fastq_gz_files.txt"
